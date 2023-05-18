ddc4668540b8f24a4dfe4866b21fc51eaa9cb94e
tube-jeonghoon <55288856+tube-jeonghoon@users.noreply.github.com>
2023-05-18 16:30:35 +0900

    최종 배포 v1.5 (#11)
    * Router.js 설정
    
    * 상세페이지에 쓸 디자인 구현중
    
    * ui 구성중
    
    * 체크인 체크아웃 구현못함..
    
    * 계정 이미지첨부 가능
    
    * 디테일페이지 미완성
    
    * 호스팅페이지 버튼에 아이콘 넣기
    
    * 카테고리, 이미지 업로드 추가
    
    * 버튼 중복 클릭 가능
    
    * 인풋 핸들러 수정
    
    * 이미지 업로드 안됨,,,
    
    * 서버연결완료
    
    * 알럿창 추가
    
    * 최종배포 v1.3 (#7)
    
    * Update issue templates
    
    * Feature/layout (#1)
    
    * readme.md 파일 수정
    
    * 중간 배포
    
    * Bug/bugfix (#3)
    
    * readme.md 파일 수정
    
    * 최종 배포 (#2)
    
    * Update issue templates
    
    * Feature/layout (#1)
    
    * readme.md 파일 수정
    
    * 중간 배포
    
    * 파일정리
    
    * Main (#5)
    
    * 최종 배포 (#2)
    
    * Update issue templates
    
    * Feature/layout (#1)
    
    * readme.md 파일 수정
    
    * 중간 배포
    
    * 파일 정리 (#4)
    
    * Update issue templates
    
    * Feature/layout (#1)
    
    * readme.md 파일 수정
    
    * 중간 배포
    
    * Bug/bugfix (#3)
    
    * readme.md 파일 수정
    
    * 최종 배포 (#2)
    
    * Update issue templates
    
    * Feature/layout (#1)
    
    * readme.md 파일 수정
    
    * 중간 배포
    
    * 파일정리
    
    * Feature/layout (#6)
    
    * readme.md 파일 수정
    
    * 최종 배포 (#2)
    
    * Update issue templates
    
    * Feature/layout (#1)
    
    * readme.md 파일 수정
    
    * 중간 배포
    
    * 파일 정리 (#4)
    
    * Update issue templates
    
    * Feature/layout (#1)
    
    * readme.md 파일 수정
    
    * 중간 배포
    
    * Bug/bugfix (#3)
    
    * readme.md 파일 수정
    
    * 최종 배포 (#2)
    
    * Update issue templates
    
    * Feature/layout (#1)
    
    * readme.md 파일 수정
    
    * 중간 배포
    
    * 파일정리
    
    * 카드에 호스트 추가
    
    * 수정
    
    * 수정
    
    * 최종 배포 v1.4 (#9)
    
    * 카드 사이즈 수정
    
    ---------
    
    Co-authored-by: tube-jeonghoon <55288856+tube-jeonghoon@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: minyoungnam <128782170+minyoungnam@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import store from "./redux/config/configStore";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Provider store={store}>
+  // <React.StrictMode>
     <App />
-  </Provider>
-);
+  //  </React.StrictMode>
+)
--- src/shared/Router.js
+++ src/shared/Router.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../pages/Home";
 import Signup from "../pages/Signup";
 import Login from "../pages/Login";
+import Hosting from "../pages/Hosting";
 
 const Router = () => {
   return (
@@ -11,6 +12,7 @@ const Router = () => {
         <Route path="/" element={<Home />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
+        <Route path='/hosting' element={<Hosting />}/>
       </Routes>
     </BrowserRouter>
   );
